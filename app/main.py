0632765d6e44595991ec5f4ca16913604a8c1789
Johan <jsebastianhzco@gmail.com>
2025-06-29 19:41:47 -0500

    Cors Config - Router customer created

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from app.database import Base, engine
 from app.routers import cliente
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Puedes usar "*" en desarrollo si quieres permitir todo
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
